6a09dd5c8f292cd2b64d272305b804a9d8d5e014
Korrigao <kiluynam.roo@gmail.com>
2024-11-01 17:21:21 -0400

    operadores aritmeticos

--- operadores/Program.cs
+++ operadores/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//Principais operadores
+
+double resultado1 = 12 + 8 + 12 / 4;
+Console.WriteLine(resultado1);
+
+
+double resultado2 = (12 + 8 + 12) / 3;
+Console.WriteLine(resultado2);
+
+
+double resultado3 = (2 + 8) * 2 / 4;
+Console.WriteLine(resultado3);
