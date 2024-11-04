eff864a3007a6ab0f9e0905a3b59a1560303cf72
Korrigao <kiluynam.roo@gmail.com>
2024-11-04 16:54:51 -0400

    Adicionado concatenação

---
+++ concatenação/Program.cs
@@ -0,0 +1,8 @@
+﻿//Concatenação - faz a junção de duas ou mais strings, ou cadeias de caractéres, em uma única string. Muita utilizada para criar URLs
+//e sentenças dinâmicas.
+string nome = "João";
+string saudacao = "Olá " + nome + ", Bem-vindo!";
+Console.WriteLine(saudacao);
+
+
+//
