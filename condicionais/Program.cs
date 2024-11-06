7c440defcc4e8529683f5503870398d59bbbfb3e
Korrigao <kiluynam.roo@gmail.com>
2024-11-06 16:30:40 -0400

    sla

--- condicionais/Program.cs
+++ condicionais/Program.cs
@@ -1,7 +1,7 @@
 ﻿//A condicional if sempre lê o primeiro bloco{}, se o primeiro bloco for FALSO ele lê o segundo bloco{}.
 double nota1 = 5.5;
 double nota2 = 6.0;
-double nota3 = 6.8;
+double nota3 = 7.0;
 double media = (nota1 + nota2 + nota3) / 3;
 if (media>=7)
 {
