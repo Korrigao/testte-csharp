68b6e374744587b766c1b9228d5f9c290fa9569a
Korrigao <kiluynam.roo@gmail.com>
2024-11-06 16:15:08 -0400

    Condicionais

---
+++ condicionais/Program.cs
@@ -0,0 +1,13 @@
+﻿//A condicional if sempre lê o primeiro bloco{}, se o primeiro bloco for FALSO ele lê o segundo bloco{}.
+double nota1 = 5.5;
+double nota2 = 6.0;
+double nota3 = 6.8;
+double media = (nota1 + nota2 + nota3) / 3;
+if (media>=7)
+{
+    Console.WriteLine($"Média: {media:F2}, Aprovado!");
+}
+else
+{
+    Console.WriteLine($"Média: {media:F2}, Recuperação.");
+}
