b307e636570617f809386a7d449ab8c25002a5ee
Korrigao <kiluynam.roo@gmail.com>
2024-11-08 14:30:41 -0400

    Switch Case

--- condicionais/Program.cs
+++ condicionais/Program.cs
@@ -34,29 +34,69 @@
 
 
 
+// Console.WriteLine("Digite uma vogal:");
+// string? letra = Console.ReadLine();
+// if (letra == "a" || letra == "A")
+// {
+//     Console.WriteLine($"Você digitou a vogal: {letra}");
+// }
+// else if (letra == "e" || letra == "E")
+// {
+//     Console.WriteLine($"Você digitou a vogal: {letra}");
+// }
+// else if (letra == "i" || letra == "I")
+// {
+//     Console.WriteLine($"Você digitou a vogal: {letra}");
+// }
+// else if (letra == "o" || letra == "O")
+// {
+//     Console.WriteLine($"Você digitou a vogal: {letra}");
+// }
+// else if (letra == "u" || letra == "U")
+// {
+//     Console.WriteLine($"Você digitou a vogal: {letra}");
+// }
+// else
+// {
+//     Console.WriteLine("Você não digitou uma vogal.");
+// }
+
+
+//Switch Case é usado quando há várias possibilidades de uma variável.
+//Espaçamento do console.WriteLine e break feito com a tecla TAB.
+// string dia = "Sábado";
+
+// switch (dia)
+// {
+//     case "Sábado":
+//         Console.WriteLine("Sábadooooooo galera!");
+//         break;
+//     case "Domingo":
+//         Console.WriteLine("Domingão, hoje é dia de assistir o malvadão");
+//         break;
+//     default:
+//         Console.WriteLine("Hoje é dia de semana, para de preguiça, levanta e vai trabalhar!");
+//         break;
+// }
+
+
+//Estutura Condicional: como vimos anteriormente podemos fazer uma série de else if s para saber a vogal, 
+//mas com o switch case é muito mais fácil:
 Console.WriteLine("Digite uma vogal:");
 string? letra = Console.ReadLine();
-if (letra == "a" || letra == "A")
+string letraminuscula = letra.ToLower();
+
+switch (letraminuscula)
 {
-    Console.WriteLine($"Você digitou a vogal: {letra}");
-}
-else if (letra == "e" || letra == "E")
-{
-    Console.WriteLine($"Você digitou a vogal: {letra}");
-}
-else if (letra == "i" || letra == "I")
-{
-    Console.WriteLine($"Você digitou a vogal: {letra}");
-}
-else if (letra == "o" || letra == "O")
-{
-    Console.WriteLine($"Você digitou a vogal: {letra}");
-}
-else if (letra == "u" || letra == "U")
-{
-    Console.WriteLine($"Você digitou a vogal: {letra}");
-}
-else
-{
-    Console.WriteLine("Você não digitou uma vogal.");
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine($"A letra '{letraminuscula}' é uma vogal.");
+        break;
+    default:
+        Console.WriteLine($"A letra ou número '{letraminuscula}' não é uma vogal");
+        break;
+
 }
