2207891bdb0dae5e053f444f62755d20d31c30d1
Korrigao <kiluynam.roo@gmail.com>
2024-11-22 14:16:20 -0400

    classes

--- classes/Program.cs
+++ classes/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Classes - molde abstrato em que todos os objetos que são da mesma classes serão iguais.
+//o nome da clase deve ser o mesmo do arquivo .cs e a primeira letra deve ser maiuscula.
+
+//atributos são as variaveis da clase que armazenam dados dentro dela.
+
+//Metodos são funções e servem manipular os atributos da classe.
+
+//objeto - instâncias de uma classe que alocam a memoria para a classe.
+
+
