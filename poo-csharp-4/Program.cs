8f70ce49562fb6d90516c2f0f713b2f9ea41d9e9
Korrigao <kiluynam.roo@gmail.com>
2024-11-27 16:02:56 -0400

    '

--- poo-csharp-4/Program.cs
+++ poo-csharp-4/Program.cs
@@ -1,4 +1,3 @@
 ﻿//abstract - foca em detalhes essenciais, escondendo os detalhes de implementação.
 //A classe abstrata não pode ser instanciada ou seja, não pode ser usada para construir objetos.
 //Ela esconde os detalhes complexos e deixa essas possibilidades para as classes derivadas.
-//
