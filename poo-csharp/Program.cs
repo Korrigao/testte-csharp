38053267cefed8f977e9a7f28e21afc706ebce8b
Korrigao <kiluynam.roo@gmail.com>
2024-11-26 22:18:54 -0400

    Update Program.cs

--- poo-csharp/Program.cs
+++ poo-csharp/Program.cs
@@ -1,4 +1,13 @@
 ﻿//Programação Orientada a objetos - um dos paradigmas da programação
+﻿using poo_01_classes_atributos_metodos_objetos.Modelo;
+
+Console.WriteLine("POO: Classes, atributos, métodos e Objetos\n");
+
+// Criando um objeto a partir da classe Veiculo
+Console.WriteLine("Criando instância da Classe Veiculo");
+Veiculo veiculo = new Veiculo("veiculo", "modelo veiculo", 2024, 10);
+veiculo.Acelerar(60);
+veiculo.Frear(20);
 
 //Encapsulamento - visa esconder os detalhes internos de uma classe
 //Todos os atributos e metodos são publicos se não expecíficados
