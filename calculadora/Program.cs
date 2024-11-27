ebb8a85d0fad8b354030d7dedadfe9aaa710f648
Korrigao <kiluynam.roo@gmail.com>
2024-11-27 17:05:37 -0400

    "calculadora"

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// int res;
+// Console.WriteLine("Digite seu número:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Digite seu outro número:");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Digite seu Símbolo + /- / * ou /");
+// string operador = Console.ReadLine();
+
+
+// switch (operador)
+//                 {
+//                     case "+":
+//                         res = num1 + num2;
+//                         Console.WriteLine("Addition:" + res);
+//                         break;
+//                     case "-":
+//                         res = num1 - num2;
+//                         Console.WriteLine("Subtraction:" + res);
+//                         break;
+//                     case "*":
+//                         res = num1 * num2;
+//                         Console.WriteLine("Multiplication:" + res);
+//                         break;
+//                     case "/":
+//                         res = num1 / num2;
+//                         Console.WriteLine("Division:" + res);
+//                         break;
+//                     default:
+//                         Console.WriteLine("Wrong input");
+//                         break;
+//                 }
+using System;
+using Calculadora_Modelo;
+
+
+Console.WriteLine("Digite seu número:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite seu outro número:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite seu Símbolo + /- / * ou /");
+string operador = Console.ReadLine();
+
+if (operador == +)
+{
+    Console.WriteLine(num1 + num2);
+}
+else if (operador == -)
+{
+    Console.WriteLine(num1 - num2);
+}
