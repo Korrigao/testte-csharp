a3562c80c1bf413fd1091b388e9246077ba7cadd
Korrigao <kiluynam.roo@gmail.com>
2024-11-27 15:06:07 -0400

    '

--- poo-csharp-4/Program.cs
+++ poo-csharp-4/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//abstract - foca em detalhes essenciais, escondendo os detalhes de implementação.
+//A classe abstrata não pode ser instanciada ou seja, não pode ser usada para construir objetos.
+//Ela esconde os detalhes complexos e deixa essas possibilidades para as classes derivadas.
+//
