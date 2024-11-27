171a2acc7181f0715dc9926f10e6349651c41d91
Korrigao <kiluynam.roo@gmail.com>
2024-11-27 14:20:01 -0400

    sobrecarga e sobrescrita

--- poo-csharp-3/Program.cs
+++ poo-csharp-3/Program.cs
@@ -1,1 +1,15 @@
-﻿//
+﻿//polimorfismo - permite que objetos de outras classes sejam tratados como objetos de uma classe comum.
+
+//sobrescrita - permite que objetos de uma clase respondam de forma diferente à mesma mensagem.
+//virtual - colocado no método para dizer que esse método pode ser sobrescrito.
+//override - indica que um metodo em classe está substituindo um método.
+
+
+
+
+
+//sobrecarga - capacidade de definir varios metodos com comportamentos diferentes, com o mesmo nome em uma classe.
+//A emplementação é necessário somente acrescentar um parâmetro no método.
+
+
+
