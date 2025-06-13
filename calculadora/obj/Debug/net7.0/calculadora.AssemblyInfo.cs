c55719da64f742c2b2df960609386dcf89520c89
Korrigao <kiluynam.roo@gmail.com>
2025-06-12 21:43:41 -0400

    ass

--- calculadora/obj/Debug/net7.0/calculadora.AssemblyInfo.cs
+++ calculadora/obj/Debug/net7.0/calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceb7c0e3e22ae98b513241227b46d7d6787921d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
